9946d8cb6a3451417303b51bff617819fa3813f7
Ted Irland <irlandth@gmail.com>
2021-06-28 15:41:01 -0500

    Added styles into details page

--- pizzatown_front/src/pages/[slug].js
+++ pizzatown_front/src/pages/[slug].js
@@ -14,8 +14,8 @@ export default function slug({ pizza, otherPizzas }) {
           src={pizza.image}
           alt={pizza.name}
           className={styles.pizzaImage}
-          height="360px"
-          width="360px"
+          height="800px"
+          width="800px"
         ></Image>
       </div>
       <div className={styles.pizzaWrapperRight}>
@@ -27,25 +27,26 @@ export default function slug({ pizza, otherPizzas }) {
             {pizza.toppings.map(topping => topping).join(', ')}
           </p>
         </div>
-      </div>
-      <div className={styles.otherPizzasWrapper}>
-        {otherPizzas.map(otherPizza => {
-          return (
-            <div className={styles.otherPizzas} key={otherPizza.id}>
-              <Link href={'/' + otherPizza.slug}>
-                <a>
-                  <Image
-                    src={otherPizza.image}
-                    alt={otherPizza.name}
-                    height="180px"
-                    width="180px"
-                  ></Image>
-                  <p>{otherPizza.name}</p>
-                </a>
-              </Link>
-            </div>
-          );
-        })}
+        <div className={styles.otherPizzasWrapper}>
+          {otherPizzas.map(otherPizza => {
+            return (
+              <div className={styles.otherPizzas} key={otherPizza.id}>
+                <Link href={'/' + otherPizza.slug}>
+                  <a>
+                    <Image
+                      className={styles.otherPizzaImg}
+                      src={otherPizza.image}
+                      alt={otherPizza.name}
+                      height="180px"
+                      width="180px"
+                    ></Image>
+                    <p>{otherPizza.name}</p>
+                  </a>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
@@ -57,6 +58,8 @@ export const getStaticPaths = async () => {
       id: '1',
       name: 'Cheese Pizza',
       slug: 'cheeze-pizza',
+      description:
+        'A delicious cheese pizza made with our finest tomato sauce.',
       toppings: ['mozarella cheese'],
       image:
         'https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80',
@@ -66,6 +69,7 @@ export const getStaticPaths = async () => {
       id: '2',
       name: 'Meat Lovers',
       slug: 'meat-lovers',
+      description: "A carnivore's delight!",
       toppings: ['ham', 'bacon', 'pepperoni', 'sausage'],
       image:
         'https://images.unsplash.com/photo-1600346019001-8d56d1b51d59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -75,6 +79,7 @@ export const getStaticPaths = async () => {
       id: '3',
       name: 'Supreme',
       slug: 'supreme',
+      description: 'A pizza fit for a king',
       toppings: ['olives', 'pineapple', 'ham', 'pepperoni'],
       image:
         'https://images.unsplash.com/photo-1617343267017-e344bdc7ec94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -84,6 +89,7 @@ export const getStaticPaths = async () => {
       id: '4',
       name: 'Pepperoni Pizza',
       slug: 'pepperoni-pizza',
+      description: 'A true classic and always a good decision',
       toppings: ['pepperoni'],
       image:
         'https://images.unsplash.com/photo-1564128442383-9201fcc740eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2002&q=80',
@@ -93,6 +99,7 @@ export const getStaticPaths = async () => {
       id: '5',
       name: 'Smoked Sausage Pizza',
       slug: 'smoked-sausage-pizza',
+      description: 'Smoke me, daddy',
       toppings: ['sausage', 'tomato', 'olive'],
       image:
         'https://images.unsplash.com/photo-1620374645310-f9d97e733268?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -102,6 +109,7 @@ export const getStaticPaths = async () => {
       id: '6',
       name: 'Egg & Sausage Pizza',
       slug: 'egg-and-sausage-pizza',
+      description: 'You got egg on my pizza!',
       toppings: ['sausage', 'egg'],
       image:
         'https://images.unsplash.com/photo-1605591099585-087b3d54cd45?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1642&q=80',
@@ -123,6 +131,8 @@ export const getStaticProps = async ({ params }) => {
       id: '1',
       name: 'Cheese Pizza',
       slug: 'cheeze-pizza',
+      description:
+        'A delicious cheese pizza made with our finest tomato sauce.',
       toppings: ['mozarella cheese'],
       image:
         'https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80',
@@ -132,6 +142,7 @@ export const getStaticProps = async ({ params }) => {
       id: '2',
       name: 'Meat Lovers',
       slug: 'meat-lovers',
+      description: "A carnivore's delight!",
       toppings: ['ham', 'bacon', 'pepperoni', 'sausage'],
       image:
         'https://images.unsplash.com/photo-1600346019001-8d56d1b51d59?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -141,6 +152,7 @@ export const getStaticProps = async ({ params }) => {
       id: '3',
       name: 'Supreme',
       slug: 'supreme',
+      description: 'A pizza fit for a king',
       toppings: ['olives', 'pineapple', 'ham', 'pepperoni'],
       image:
         'https://images.unsplash.com/photo-1617343267017-e344bdc7ec94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -150,6 +162,7 @@ export const getStaticProps = async ({ params }) => {
       id: '4',
       name: 'Pepperoni Pizza',
       slug: 'pepperoni-pizza',
+      description: 'A true classic and always a good decision',
       toppings: ['pepperoni'],
       image:
         'https://images.unsplash.com/photo-1564128442383-9201fcc740eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2002&q=80',
@@ -159,6 +172,7 @@ export const getStaticProps = async ({ params }) => {
       id: '5',
       name: 'Smoked Sausage Pizza',
       slug: 'smoked-sausage-pizza',
+      description: 'Smoke me, daddy',
       toppings: ['sausage', 'tomato', 'olive'],
       image:
         'https://images.unsplash.com/photo-1620374645310-f9d97e733268?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
@@ -168,6 +182,7 @@ export const getStaticProps = async ({ params }) => {
       id: '6',
       name: 'Egg & Sausage Pizza',
       slug: 'egg-and-sausage-pizza',
+      description: 'Hey! you got egg in my pizza!',
       toppings: ['sausage', 'egg'],
       image:
         'https://images.unsplash.com/photo-1605591099585-087b3d54cd45?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1642&q=80',
